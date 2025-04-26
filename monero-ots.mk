fa14aab7dafa32f58b4247970e2cea4f37f871cd
who asks? <user@localhost>
2025-04-26 14:24:52 -0600

    change to ots-{version}.tar.gz from otslib-{version}.tar.gz, fixing dependencies

--- monero-ots.mk
+++ monero-ots.mk
@@ -5,17 +5,29 @@
 ################################################################################
 
 MONERO_OTS_VERSION = 0.1.0
-MONERO_OTS_SOURCE = otslib-$(MONERO_OTS_VERSION).tar.gz
+MONERO_OTS_SOURCE = ots-$(MONERO_OTS_VERSION).tar.gz
 MONERO_OTS_SITE = https://github.com/DiosDelRayo/monero/archive/refs/tags
 MONERO_OTS_LICENSE = BipCot-1.3
 MONERO_OTS_LICENSE_FILES = LICENSE
 MONERO_OTS_DEPENDENCIES = boost openssl
+MONERO_OTS_DEPENDENCIES += $(if $(BR2_PACKAGE_BOOST_FILESYSTEM),boost-filesystem)
+MONERO_OTS_DEPENDENCIES += $(if $(BR2_PACKAGE_BOOST_THREAD),boost-thread)
+MONERO_OTS_DEPENDENCIES += $(if $(BR2_PACKAGE_BOOST_SYSTEM),boost-system)
+MONERO_OTS_DEPENDENCIES += $(if $(BR2_PACKAGE_BOOST_CHRONO),boost-chrono)
+MONERO_OTS_DEPENDENCIES += $(if $(BR2_PACKAGE_BOOST_SERIALIZATION),boost-serialization)
 MONERO_OTS_SUBDIR = ots
 
 # We need to include the Monero source directory
 MONERO_OTS_CONF_OPTS = -DMONERO_SOURCE_DIR=$(MONERO_OTS_SRCDIR)
 
 # Options
+MONERO_OTS_CONF_OPTS += -DBoost_FILESYSTEM_LIBRARY=$(STAGING_DIR)/usr/lib/libboost_filesystem.a
+MONERO_OTS_CONF_OPTS += -DBoost_THREAD_LIBRARY=$(STAGING_DIR)/usr/lib/libboost_thread.a
+MONERO_OTS_CONF_OPTS += -DBoost_SYSTEM_LIBRARY=$(STAGING_DIR)/usr/lib/libboost_system.a
+MONERO_OTS_CONF_OPTS += -DBoost_CHRONO_LIBRARY=$(STAGING_DIR)/usr/lib/libboost_chrono.a
+MONERO_OTS_CONF_OPTS += -DBoost_SERIALIZATION_LIBRARY=$(STAGING_DIR)/usr/lib/libboost_serialization.a
+MONERO_OTS_CONF_OPTS += -DBoost_INCLUDE_DIR=$(STAGING_DIR)/usr/include
+
 MONERO_OTS_CONF_OPTS += -DBUILD_SHARED_LIBS=OFF
 MONERO_OTS_CONF_OPTS += -DBUILD_TESTS=OFF
 MONERO_OTS_CONF_OPTS += -DBUILD_DOCS=OFF
