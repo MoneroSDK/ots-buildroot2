912a2187f36e36868a320e0dd6c914d0d606c029
who asks? <user@localhost>
2026-04-28 16:33:21 -0600

    otslib-0.3.0

--- monero-ots.mk
+++ monero-ots.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MONERO_OTS_VERSION = 0.1.0
+MONERO_OTS_VERSION = 0.3.0
 MONERO_OTS_SOURCE = otslib-$(MONERO_OTS_VERSION).tar.gz
 MONERO_OTS_SITE = https://github.com/DiosDelRayo/monero/archive/refs/tags
 MONERO_OTS_LICENSE = BipCot-1.3
@@ -16,12 +16,7 @@ MONERO_OTS_SUBDIR = ots
 MONERO_OTS_CONF_OPTS = -DMONERO_SOURCE_DIR=$(MONERO_OTS_SRCDIR)
 
 # Options
-MONERO_OTS_CONF_OPTS += -DBUILD_SHARED_LIBS=OFF
-MONERO_OTS_CONF_OPTS += -DBUILD_TESTS=OFF
-MONERO_OTS_CONF_OPTS += -DBUILD_DOCS=OFF
-MONERO_OTS_CONF_OPTS += -DBUILD_V1SIGN=OFF
-MONERO_OTS_CONF_OPTS += -DBUILD_BLOCKTIME=OFF
-MONERO_OTS_CONF_OPTS += -DBUILD_BIN_HEADER_MACRO=OFF
+MONERO_OTS_CONF_OPTS += -DBUILD_SHARED_LIBS=ON
 
 # Use ccache if enabled
 ifeq ($(BR2_CCACHE),y)
@@ -31,8 +26,6 @@ endif
 # Debug build
 ifeq ($(BR2_ENABLE_DEBUG),y)
 MONERO_OTS_CONF_OPTS += -DDEBUG=ON
-else
-MONERO_OTS_CONF_OPTS += -DDEBUG=OFF
 endif
 
 $(eval $(cmake-package))
